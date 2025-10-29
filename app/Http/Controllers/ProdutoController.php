02f46d6a7e2d2c62c4f29bdfabbf28e165be1e4a
Pedro Lucas <plfm3k@gmail.com>
2025-10-28 22:15:28 -0400

    Adicionado controllers iniciais

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProdutoRequest;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $produtos = Produto::with('categoria')->get();
+        return view('produtos.index', compact('produtos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $categorias = Categoria::all();
+        return view('produtos.create', compact('categorias'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(produtoRequest $request)
+    {
+        Produto::create($request->validated());
+        return redirect()->route('produtos.index')->with('success', 'produto cadastrada com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $produto = Produto::findOrFail($id);
+        return view('produtos.show', compact('produto'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $produto = Produto::findOrFail($id);
+        $categorias = Categoria::all();
+        return view('produtos.edit', compact('produto', 'cateogiras'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(produtoRequest $request, string $id)
+    {
+        $produto = Produto::findOrFail($id);
+        $produto->update($request->validated());
+        return redirect()->route('produtos.index')->with('success', 'produto atualizada com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $produto = Produto::findOrFail($id);
+        $produto->destroy();
+        return redirect()->route('produtos.index')->with('success', 'produto excluída com sucesso');
+    }
+}
