21b9fc390f15bcdbf3405168c0fa0d5ddfcd72f8
Pedro Lucas <plfm3k@gmail.com>
2025-10-29 19:32:58 -0400

    Adicionado controllers Categoria e Compra

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,9 +2,71 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CategoriaRequest;
 
 class CategoriaController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categorias = Categoria::all();
+        return view ('categorias.index', compact('categorias'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('categorias.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoriaRequest $request)
+    {
+        Categoria::create($request->validated());
+        return redirect()->route('categorias.index')->with('success', 'Categoria criada com sucesso!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        return view('categorias.show', compact('categoria'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        return view('categorias.edit', compact('categoria'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoriaRequest $request, string $id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        $categoria->update($request->validated());
+        return redirect()->route('categorias.index')->with('success', 'Categoria editada com sucesso!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        $categoria->destroy();
+        return redirect()->route('categorias.index')->with('success', 'Categoria excluída com sucesso!');
+    }
 }
