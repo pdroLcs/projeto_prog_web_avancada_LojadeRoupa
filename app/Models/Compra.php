adb1518647e1b9e3b281c50d5aa47e3605008688
Luiz Gustavo <165921842+LGDAA@users.noreply.github.com>
2025-11-01 18:02:05 -0300

    Update do model Compra.php
    Adição dos campos fillable

--- app/Models/Compra.php
+++ app/Models/Compra.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Compra extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['valor_total', 'status'];
+
 }
