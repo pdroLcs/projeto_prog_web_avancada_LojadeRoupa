d48e7dc98fde39a70a4f71761948dcb1774c9e8f
Pedro Lucas <plfm3k@gmail.com>
2025-11-10 12:53:48 -0400

    Atualiza MensagemController e MensagemRequest

--- app/Http/Controllers/MensagemController.php
+++ app/Http/Controllers/MensagemController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\MensagemRequest;
 use App\Models\Cliente;
-use Illuminate\Http\Request;
+use App\Models\Mensagem;
 
 class MensagemController extends Controller
 {
--- app/Http/Requests/MensagemRequest.php
+++ app/Http/Requests/MensagemRequest.php
@@ -11,7 +11,7 @@ class MensagemRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,16 @@ class MensagemRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'assunto' => 'string|max:100',
+            'mensagem' => 'required|string|max:1000',
+            'cliente_id' => 'required|exists:cliente,id'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'mensagem.required' => 'Por favor, insira uma mensagem!'
         ];
     }
 }
