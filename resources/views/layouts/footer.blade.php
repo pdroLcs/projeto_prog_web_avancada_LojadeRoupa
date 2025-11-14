9ff35c6f1c5a1f2c268ba4feafb90d3bdf3db570
Guilherme Emannuel <guilherme.emannuel05@gmail.com>
2025-11-13 23:10:56 -0400

    feat: Adicionei footer com 'Contato' e 'Localização' estilizados

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,37 @@
-<footer>
+<footer class="bg-dark text-white pt-2 pb-1 mt-auto">
     <div class="container">
-        <p>© {{ date('Y') }} Loja de Roupa - Todos os direitos reservados.</p>
+        <div class="row">
+
+            <div class="col-md-6 mb-3">
+                <h5 class="text-info pb-2 pt-2 text-center">Contato</h5>
+                <div class="d-flex flex-column justify-content-center h-100">
+
+                    <div class="text-center">
+                        <ul class="list-unstyled d-inline-block mx-auto">
+                            <li><i class="bi bi-geo-alt-fill me-2"></i> Endereço: Rua Exemplo, 123, Corumbá/MS</li>
+                            <li><i class="bi bi-telephone-fill me-2"></i> Telefone: +55 (67) 99934-1014</li>
+                            <li><i class="bi bi-envelope-fill me-2"></i> E-mail: Loja-de-roupa@gmail.com</li>
+                        </ul>
+
+                        <a href="https://wa.me/+5567999341014" target="_blank"
+                           class="btn btn-success btn-sm mt-2 d-block mx-auto" style="width: fit-content;">
+                            <i class="bi bi-whatsapp me-2"></i> Fale Conosco Imediatamente
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 mb-3">
+                <h5 class="text-info pb-2 pt-2 text-center">Localização (Mapa)</h5>
+                <div class="d-flex flex-column justify-content-center h-100">
+                    <div class="border bg-secondary rounded d-flex justify-content-center align-items-center text-light map-placeholder w-100">
+                        <iframe class="w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3772.4276176691433!2d-57.63055!3d-19.0008697!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93870a650eaca20d%3A0xeb1f97231daad040!2sUniversidade%20Federal%20de%20Mato%20Grosso%20do%20Sul%20-%20C%C3%A2mpus%20do%20Pantanal!5e0!3m2!1spt-BR!2sbr!4v1763089597259!5m2!1spt-BR!2sbr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="text-center mt-3 border-top pt-2">
+            <small>© {{ date('Y') }} Organização Fictícia. Todos os direitos reservados.</small>
+        </div>
     </div>
 </footer>
