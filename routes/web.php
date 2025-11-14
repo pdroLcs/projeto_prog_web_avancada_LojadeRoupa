6bf81cc7c308d7a5574b23af58c6bfe66612671c
Pedro Lucas <plfm3k@gmail.com>
2025-11-13 21:07:18 -0400

    Acesso correto das views produtos

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('compras', CompraController::class);
 });
 
 require __DIR__.'/auth.php';
@@ -28,6 +29,5 @@ Route::middleware(['auth', 'admin'])->group(function() {
     Route::resource('produtos', ProdutoController::class)->except('index', 'show');
 });
 Route::resource('produtos', ProdutoController::class)->only('index', 'show');
-Route::resource('compras', CompraController::class);
 Route::resource('clientes', ClienteController::class);
 Route::view('/', 'boas_vindas')->name('home');
